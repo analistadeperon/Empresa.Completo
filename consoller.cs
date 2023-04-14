e1322f365ceaa7cc40d080f8bee4a63e8b8b5970
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2023-04-14 10:04:49 -0300

    Create consoller.cs

---
+++ consoller.cs
@@ -0,0 +1,41 @@
+<Dados>
+  <Estudante Nome="Matheus"> <!-- Node com mesmo nome-->
+    <Endereco>Rua Projetada 100</Endereco>
+    <Email>macoratti@yahoo.com</Email>
+      <Cidade>Lins</Cidade>
+      <Estado>SP</Estado>
+  </Estudante>
+  <Estudante Nome="BOM">
+    <Email>
+      monica@bol.com.br
+      </Email>
+      <Endereco>Av. XV de Novembro 200</Endereco>
+      <Cidade>Santos</Cidade>
+      <Estado>SP</Estado>
+    </Estudante>
+  <Estudante Nome="Dia">
+    <Email>
+      bibi@hotmail.com
+      </Email>
+      <Endereco>Rua Marco10 300</Endereco>
+      <Cidade>Xerem</Cidade>
+      <Estado>RJ</Estado>
+    </Estudante>
+  <Estudante Nome="Pedro">
+    <Email>
+      pedro@bol.com.br
+      </Email>
+      <Endereco>Rua Peru 23</Endereco>
+      <Cidade>Cristalina</Cidade>
+      <Estado>GO</Estado>
+    </Estudante>
+  <Estudante Nome="JOM">
+    <!--Node com mesmo nome-->
+    <Endereco>Rua Equador 98</Endereco>
+    <Email>
+      macoratti@yahoo.com
+    </Email>
+      <Cidade>Camprinas</Cidade>
+      <Estado>SP</Estado>
+    </Estudante>
+</Dados>
